41771b0877aa3603982c42ef594f9a528dd18238
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-08-10 19:31:47 +0200

    Start ReviveBeacon

---
+++ src/main/kotlin/de/jonasheilig/heartGame/listeners/ReviveBeaconListener.kt
@@ -0,0 +1,69 @@
+package de.jonasheilig.heartGame.listeners
+
+import de.jonasheilig.heartGame.items.ReviveBeacon
+import de.jonasheilig.heartGame.mode.SurvivalSpectatorMode
+import de.jonasheilig.heartGame.ui.showReviveUI
+import org.bukkit.*
+import org.bukkit.block.Block
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.meta.SkullMeta
+import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.util.Vector
+
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+
+
+class ReviveBeaconListener(private val plugin: Plugin) : Listener {
+
+    @EventHandler
+    fun onBlockPlace(event: BlockPlaceEvent) {
+        val player = event.player
+        val block = event.block
+        val item = event.itemInHand
+
+        if (ReviveBeacon.isReviveBeacon(item)) {
+            val blockType = block.type
+            if (blockType == Material.BEACON && checkDiamondFrameAround(block)) {
+                startReviveAnimation(block.location)
+
+                showReviveUI(player, plugin)
+
+                object : BukkitRunnable() {
+                    override fun run() {
+                        executeRevive(block.location, player)
+                    }
+                }.runTaskLater(plugin, 20 * 20)
+            }
+        }
+    }
+
+    @EventHandler
+    fun onInventoryClick(event: InventoryClickEvent) {
+        val player = event.whoClicked as? Player ?: return
+        val clickedItem = event.currentItem ?: return
+
+        if (event.view.title() == Component.text("Select a Player to Revive").color(NamedTextColor.GREEN)) {
+            event.isCancelled = true
+
+            if (clickedItem.type == Material.PLAYER_HEAD) {
+                val meta = clickedItem.itemMeta as SkullMeta
+                val targetComponent = meta.displayName() ?: return
+                val targetName = LegacyComponentSerializer.legacySection().serialize(targetComponent)
+                val target = Bukkit.getPlayer(targetName)
+
+                if (target != null && SurvivalSpectatorMode(plugin).isInSpectatorMode(target)) {
+                    revivePlayer(target, player)
+                    player.sendMessage("${target.name} wurde erfolgreich wiederbelebt!")
+                    player.closeInventory()
+                }
+            }
+        }
+    }
+}
