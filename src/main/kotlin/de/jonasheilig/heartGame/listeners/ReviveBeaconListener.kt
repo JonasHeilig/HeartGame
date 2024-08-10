396da374cfa57ee38f3b19e9f2b7141b47dd77f7
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-08-10 19:34:35 +0200

    Finish ReviveBeaconListener

--- src/main/kotlin/de/jonasheilig/heartGame/listeners/ReviveBeaconListener.kt
+++ src/main/kotlin/de/jonasheilig/heartGame/listeners/ReviveBeaconListener.kt
@@ -19,7 +19,6 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 
-
 class ReviveBeaconListener(private val plugin: Plugin) : Listener {
 
     @EventHandler
@@ -62,6 +61,73 @@ class ReviveBeaconListener(private val plugin: Plugin) : Listener {
                     revivePlayer(target, player)
                     player.sendMessage("${target.name} wurde erfolgreich wiederbelebt!")
                     player.closeInventory()
+                }
+            }
+        }
+    }
+
+    private fun revivePlayer(target: Player, reviver: Player) {
+        val location = reviver.location
+        location.block.type = Material.AIR
+        for (x in -1..1) {
+            for (y in -1..1) {
+                for (z in -1..1) {
+                    val block = location.clone().add(x.toDouble(), y.toDouble(), z.toDouble()).block
+                    if (block.type == Material.DIAMOND_BLOCK) {
+                        block.type = Material.AIR
+                    }
+                }
+            }
+        }
+
+        val spectatorMode = SurvivalSpectatorMode(plugin)
+        spectatorMode.toggleSurvivalSpectatorMode(target)
+        target.gameMode = GameMode.SURVIVAL
+        target.health = 20.0
+        target.sendMessage("Du wurdest von ${reviver.name} wiederbelebt!")
+    }
+
+    private fun checkDiamondFrameAround(centerBlock: Block): Boolean {
+        val directions = arrayOf(
+            Vector(1, 0, 0), Vector(-1, 0, 0),
+            Vector(0, 0, 1), Vector(0, 0, -1),
+            Vector(1, 0, 1), Vector(-1, 0, -1),
+            Vector(-1, 0, 1), Vector(1, 0, -1)
+        )
+        return directions.all {
+            val block = centerBlock.getRelative(it.blockX, it.blockY, it.blockZ)
+            block.type == Material.DIAMOND_BLOCK
+        }
+    }
+
+    private fun startReviveAnimation(location: Location) {
+        object : BukkitRunnable() {
+            override fun run() {
+                val radius = 5.0
+                val center = location.clone().add(0.5, 1.0, 0.5)
+                for (angle in 0..360 step 15) {
+                    val radians = Math.toRadians(angle.toDouble())
+                    val x = radius * Math.cos(radians)
+                    val z = radius * Math.sin(radians)
+                    val particleLocation = center.clone().add(x, 0.0, z)
+                    location.world?.spawnParticle(Particle.END_ROD, particleLocation, 1)
+                }
+            }
+        }.runTaskTimer(plugin, 0L, 5L)
+    }
+
+    private fun executeRevive(location: Location, player: Player) {
+        val world = location.world
+        world?.let {
+            location.block.type = Material.AIR
+            for (x in -1..1) {
+                for (y in -1..1) {
+                    for (z in -1..1) {
+                        val block = location.clone().add(x.toDouble(), y.toDouble(), z.toDouble()).block
+                        if (block.type == Material.DIAMOND_BLOCK) {
+                            block.type = Material.AIR
+                        }
+                    }
                 }
             }
         }
